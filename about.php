5842f1618d828382ee0c3f82bc7cac6c2edb89da
Ahmet Çardak <ahmetcardak@Ahmet-MacBook-Air-2.local>
2024-01-01 20:29:59 +0300

    m

---
+++ about.php
@@ -0,0 +1,39 @@
+<?php require "inc/ust.php"; ?>
+
+    <!-- Page Header Start -->
+    <div class="container-fluid page-header py-5 mb-5">
+        <div class="container py-5">
+            <h1 class="display-3 text-white mb-3 animated slideInDown">Hakkımızda</h1>
+        </div>
+    </div>
+    <!-- Page Header End -->
+
+<div class="container mt-5">
+
+    <div class="row">
+        <div class="col m-1">
+            <h2 class="about_head text-center">HAKKIMIZDA</h2>
+            <p class="about-text text-center">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi mollitia odit temporibus. Autem debitis dignissimos ea eveniet id iste molestias odit, veritatis. At in ipsa numquam optio, pariatur quas quibusdam.
+            </p>
+        </div>
+    </div>
+
+    <div class="row mt-3">
+        <div class="col-lg-6 col-md-6 col-sm-12 about_bg">
+            <h2 class="about_head">VİZYONUMUZ</h2>
+            <p class="about-text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid animi atque expedita ipsum iste magni, maxime nulla provident quam quis recusandae reprehenderit, sint velit. Assumenda eius mollitia non quos tenetur?
+            </p>
+        </div>
+        <div class="col-1"></div>
+        <div class="col-lg-5 col-md-5 col-sm-12 text-end about_bg_2">
+            <h2 class="about_head">VİZYONUMUZ</h2>
+            <p class="about-text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid animi atque expedita ipsum iste magni, maxime nulla provident quam quis recusandae reprehenderit, sint velit. Assumenda eius mollitia non quos tenetur?
+            </p>
+        </div>
+    </div>
+    
+</div>
+<?php require "inc/alt.php"; ?>
---
+++ services.php
@@ -0,0 +1,95 @@
+<?php require "inc/ust.php"; ?>
+
+<!-- Page Header Start -->
+<div class="container-fluid page-header py-5 mb-5">
+    <div class="container py-5">
+        <h1 class="display-3 text-white mb-3 animated slideInDown">Hizmetlerimiz</h1>
+    </div>
+</div>
+<!-- Page Header End -->
+
+<!-- Service Start -->
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="section-title text-center">
+            <h1 class="display-5 mb-5">Hizmetlerimiz</h1>
+        </div>
+        <div class="row g-4">
+            <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="service-item">
+                    <div class="overflow-hidden">
+                        <img class="img-fluid" src="img/service-1.jpg" alt="">
+                    </div>
+                    <div class="p-4 text-center border border-5 border-light border-top-0">
+                        <h4 class="mb-3">General Carpentry</h4>
+                        <p>Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem diam.</p>
+                        <a class="fw-medium" href="">Read More<i class="fa fa-arrow-right ms-2"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.3s">
+                <div class="service-item">
+                    <div class="overflow-hidden">
+                        <img class="img-fluid" src="img/service-2.jpg" alt="">
+                    </div>
+                    <div class="p-4 text-center border border-5 border-light border-top-0">
+                        <h4 class="mb-3">Furniture Manufacturing</h4>
+                        <p>Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem diam.</p>
+                        <a class="fw-medium" href="">Read More<i class="fa fa-arrow-right ms-2"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.5s">
+                <div class="service-item">
+                    <div class="overflow-hidden">
+                        <img class="img-fluid" src="img/service-3.jpg" alt="">
+                    </div>
+                    <div class="p-4 text-center border border-5 border-light border-top-0">
+                        <h4 class="mb-3">Furniture Remodeling</h4>
+                        <p>Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem diam.</p>
+                        <a class="fw-medium" href="">Read More<i class="fa fa-arrow-right ms-2"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="service-item">
+                    <div class="overflow-hidden">
+                        <img class="img-fluid" src="img/service-4.jpg" alt="">
+                    </div>
+                    <div class="p-4 text-center border border-5 border-light border-top-0">
+                        <h4 class="mb-3">Wooden Floor</h4>
+                        <p>Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem diam.</p>
+                        <a class="fw-medium" href="">Read More<i class="fa fa-arrow-right ms-2"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.3s">
+                <div class="service-item">
+                    <div class="overflow-hidden">
+                        <img class="img-fluid" src="img/service-5.jpg" alt="">
+                    </div>
+                    <div class="p-4 text-center border border-5 border-light border-top-0">
+                        <h4 class="mb-3">Wooden Furniture</h4>
+                        <p>Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem diam.</p>
+                        <a class="fw-medium" href="">Read More<i class="fa fa-arrow-right ms-2"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.5s">
+                <div class="service-item">
+                    <div class="overflow-hidden">
+                        <img class="img-fluid" src="img/service-6.jpg" alt="">
+                    </div>
+                    <div class="p-4 text-center border border-5 border-light border-top-0">
+                        <h4 class="mb-3">Custom Work</h4>
+                        <p>Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem diam.</p>
+                        <a class="fw-medium" href="">Read More<i class="fa fa-arrow-right ms-2"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Service End -->
+
+<?php require "inc/alt.php"; ?>
